3846e8447ddc2fc367a6dc3deb10bccf872f5a3f
BrayanMartinez29 <brayanmartinezpatron@gmail.com>
2026-09-13 22:11:56 -0500

    Add domain entities, AppDbContext and initial migration (Module 2)

---
+++ GolBet.Entities/Bet.cs
@@ -0,0 +1,21 @@
+﻿// GolBet.Entities/Bet.cs
+using System.ComponentModel.DataAnnotations.Schema;
+using GolBet.Entities.Common;
+using GolBet.Entities.Enums;
+
+namespace GolBet.Entities;
+
+public class Bet : AuditableEntity
+{
+    [Column(TypeName = "decimal(12,2)")]
+    public decimal Amount { get; set; }
+
+    [Column(TypeName = "decimal(5,2)")]
+    public decimal OddsAtPlacement { get; set; }
+
+    public BetPick Pick { get; set; }
+    public BetStatus Status { get; set; } = BetStatus.Pending;
+
+    public int MatchId { get; set; }
+    public Match Match { get; set; } = null!;
+}
---
+++ GolBet.Entities/Common/AuditableEntity.cs
@@ -0,0 +1,10 @@
+﻿// GolBet.Entities/Common/AuditableEntity.cs
+namespace GolBet.Entities.Common;
+
+public abstract class AuditableEntity
+{
+    public int Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? ModifiedDate { get; set; }
+    public bool IsActive { get; set; } = true;
+}
